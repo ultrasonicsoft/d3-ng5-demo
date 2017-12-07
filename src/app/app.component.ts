4fd5a62ca7512b8b0659533d961c431d6ee3246b
ultrasonicsoft <balramchavan@gmail.com>
2017-12-07 07:45:26 +0530

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as d3 from "d3";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngAfterContentInit(){    
+    d3.select("p").style("color", "red");
+  }
+
+  colorMe(){
+    d3.select("button").style("color", "red");
+  }
 }
