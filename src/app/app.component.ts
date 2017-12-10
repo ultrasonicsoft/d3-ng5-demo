1d4acee04f38c6a708d95ce68d1c1eb5b4358560
ultrasonicsoft <balramchavan@gmail.com>
2017-12-10 23:03:09 +0530

    Resolving "this" correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,23 @@ import * as d3 from "d3";
 export class AppComponent {
   title = 'app';
 
-  ngAfterContentInit(){    
+  radius = 10;
+
+  ngAfterContentInit() {
     d3.select("p").style("color", "red");
   }
 
-  colorMe(){
+  colorMe() {
     d3.select("button").style("color", "red");
+  }
+
+  clicked(event: any) {
+    d3.select(event.target).
+      append('circle')
+      .attr('cx', event.x)
+      .attr('cy', event.y)
+      .attr('r', () => { return this.radius; })
+      .attr('fill', 'red')
+
   }
 }
